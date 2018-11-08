0d3f38fcfe2cead9483d1f035cd2e2ce8855f189
wink <liuhang0316@163.com>
2018-11-08 23:55:50 +0800

    1111

--- src/main/java/org/com/wink/ClassLoader/SPI/Test.java
+++ src/main/java/org/com/wink/ClassLoader/SPI/Test.java
@@ -6,6 +6,10 @@ public class Test {
 
     public static void main(String[] args) throws Exception{
 
+        serviceLoaderTest();
+    }
+
+    public static void serviceLoaderTest(){
         ServiceLoader<Dog> dogs = ServiceLoader.load(Dog.class);
         for (Dog dog : dogs) {
             dog.dosomething();
