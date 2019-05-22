b53c7c7fc96e572b1d4770e99644795db9dd6410
wink <liuhang0316@163.com>
2019-05-22 23:41:53 +0800

    increment 原子性增加

--- src/main/java/org/com/wink/thread/Increment.java
+++ src/main/java/org/com/wink/thread/Increment.java
@@ -1,6 +1,43 @@
 package org.com.wink.thread;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Increment {
 
+    private Integer i = 0;
+
+    public synchronized void increat(){
+        i++;
+    }
+
+    public Integer getI() {
+        return i;
+    }
+
+    public static void main(String[] args) {
+        Increment increment = new Increment();
+
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+                Thread thread = new Thread(() -> {
+                    for (int j = 0; j < 10000; j++) {
+                        increment.increat();
+                    }
+                },"thread"+i);
+            threads.add(thread);
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.start();//???线程的启动放到这就没有问题???
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("最后结果：i = " + increment.getI());
+    }
 
 }
